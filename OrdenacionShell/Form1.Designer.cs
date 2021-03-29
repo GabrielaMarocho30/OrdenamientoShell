dbf4972f3608e35505c6d942b2eb2d4c9fd1e05e
Gabriela <020101560i@uandina.edu.pe>
2021-03-28 22:09:50 -0500

    modificando

---
+++ OrdenacionShell/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace OrdenacionShell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lstOriginal = new System.Windows.Forms.ListBox();
+            this.lstOrdenado = new System.Windows.Forms.ListBox();
+            this.txtNro = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(172, 34);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(172, 186);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 1;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.UseWaitCursor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(12, 318);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lstOriginal
+            // 
+            this.lstOriginal.FormattingEnabled = true;
+            this.lstOriginal.Location = new System.Drawing.Point(12, 75);
+            this.lstOriginal.Name = "lstOriginal";
+            this.lstOriginal.Size = new System.Drawing.Size(142, 225);
+            this.lstOriginal.TabIndex = 3;
+            // 
+            // lstOrdenado
+            // 
+            this.lstOrdenado.FormattingEnabled = true;
+            this.lstOrdenado.Location = new System.Drawing.Point(282, 75);
+            this.lstOrdenado.Name = "lstOrdenado";
+            this.lstOrdenado.Size = new System.Drawing.Size(144, 225);
+            this.lstOrdenado.TabIndex = 4;
+            // 
+            // txtNro
+            // 
+            this.txtNro.Location = new System.Drawing.Point(12, 34);
+            this.txtNro.Name = "txtNro";
+            this.txtNro.Size = new System.Drawing.Size(142, 20);
+            this.txtNro.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 371);
+            this.Controls.Add(this.txtNro);
+            this.Controls.Add(this.lstOrdenado);
+            this.Controls.Add(this.lstOriginal);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ListBox lstOriginal;
+        private System.Windows.Forms.ListBox lstOrdenado;
+        private System.Windows.Forms.TextBox txtNro;
+    }
+}
+
---
+++ OrdenacionShell/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenacionShell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Agregar un numero a la lista
+            // Declarar la variable
+            int nro;
+            // Leer nro
+            nro = int.Parse(txtNro.Text);
+            //Llevar este numero a la lista
+            lstOriginal.Items.Add(nro);
+            //Limpiar la caja de texto
+            txtNro.Clear();
+            txtNro.Focus();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            int intervalo, i, j, k, temp;
+            int[] arreglo;
+            arreglo = new int[lstOriginal.Items.Count];
+            for (int n = 0; n < lstOriginal.Items.Count; ++n)
+            {
+                lstOriginal.SetSelected(n, true);
+                txtNro.Text = lstOriginal.SelectedItem.ToString();
+                arreglo[n] = int.Parse(txtNro.Text);
+                txtNro.Clear();
+                txtNro.Focus();
+            }
+            intervalo = lstOriginal.Items.Count / 2;
+            while (intervalo >= 1)
+            {
+                for (i = 1; i < lstOriginal.Items.Count; ++i)
+                {
+                    j = i - intervalo;
+                    while (j >= 0)
+                    {
+                        k = j + intervalo;
+                        if (arreglo[j] <= arreglo[k])
+                        {
+                            j = -1;
+                        }
+                        else
+                        {
+                            temp = arreglo[j];
+                            arreglo[j] = arreglo[k];
+                            arreglo[k] = temp;
+                            j -= intervalo;
+                        }
+                    }
+                }
+                intervalo /= 2;
+            }
+            for (int n = 0; n < lstOriginal.Items.Count; ++n)
+            {
+                lstOrdenado.Items.Add(arreglo[n]);
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lstOrdenado.Items.Clear();
+            lstOriginal.Items.Clear();
+        }
+    }
+}
